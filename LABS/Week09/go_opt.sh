67005b0a38bf32e8453f46879fb6143aacd076bb
s3656673 <s3656673@student.rmit.edu.au>
2018-09-18 15:12:49 +1000

    Test getopts

---
+++ LABS/Week09/go_opt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while getopts ":a" opt; do
+case $opt in
+a)
+echo "-a was triggered!" >&2
+;;
+\?)
+echo "Invalid option: -$OPTARG" >&2
+;;
+esac
+done
---
+++ LABS/Week09/go_opt2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while getopts ":abc:d:" opt; do
+case $opt in
+a)
+echo "-a was triggered!" >&2
+;;
+b)
+echo "-b was triggered!" >&2
+;;
+c) "-c was triggered!" >&2
+echo $OPTARG
+;;
+d)
+echo "-d was triggered" >&2
+echo $OPTARG
+;;
+\?)
+echo "Invalid option: -$OPTARG" >&2
+;;
+esac
+done
