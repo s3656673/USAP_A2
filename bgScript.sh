97578838da47b7cc03da2cda75e15ea57675ca64
s3656673 <s3656673@student.rmit.edu.au>
2018-10-09 14:44:04 +1100

    Added search functionality to background script

--- bgScript.sh
+++ bgScript.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
+#Background script to find processes.
 
-#Background script to record background process.
-#ps -A | grep chromium | awk '{ print $1, $4 }'
+#Assign the given process name argument into a variable
+processName=$1
 
-#Global variables
+#Declare global variables
 found=false
 
-#Check if the process can be found
-if [ -z `ps -A | grep chromium` ]
+#Check if process name exists
+pgrep "$processName" > /dev/null && found=true
+pgrep "$processName" > /dev/null || found=false
+
+if [ "$found" == "false" ]
 then
-found=false
-else
-found=true
+echo "process not found"
 fi
 
-#If found, present the information to the user
+#Invoke the PS command and return the results with the process name
 if [ "$found" == "true" ]
 then
-ps -A | grep chromium | awk '{ print $1, $4 }'
-else
-echo "Could not find the request"
+#print results to screen
+
+#store values into an array
+IFS=$(echo -en "\n\b")
+targets=$(ps -A | grep $processName | awk '{ print $1, $4 }')
+num=1
+echo "$targets" > testfile.txt
+IFS=,$'\n' read -d '' -r -a processArray <testfile.txt
+
+#Print array elements to screen
+num=1
+for each in "${processArray[@]}"
+do
+echo "$num. $each"
+let "num=num+1"
+done
 fi
